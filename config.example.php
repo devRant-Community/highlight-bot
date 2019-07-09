9c7ce8fdb87d42977155fb06c2eb369d8de482eb
Skayo <jonasg.cool@gmail.com>
2019-07-09 18:16:09 +0200

    Update config.example.php

--- config.example.php
+++ config.example.php
@@ -1,7 +1,13 @@
 <?php
 
-define('DEVRANT_API', 'https://devrant.com/api/');
+define('DEVRANT_API', 'https://devrant.com/api');
 define('DEVRANT_USERNAME', 'highlight');
-define('DEVRANT_PASSWORD', 'let_me_iiinnn');
+define('DEVRANT_PASSWORD', 'let_me_iiiiinnnnn');
+define('DEVRANT_AUTH_TOKEN_FILE', 'auth-token.json');
 
 define('DEBUG', true);
+
+define('BOT_LAST_CHECK_TIME_FILE', 'lastCheckTime.txt');
+
+define('THEME_SELECTION_RANT_ID', false);
+define('THEME_SELECTION_FILE', 'theme-selection.json');
