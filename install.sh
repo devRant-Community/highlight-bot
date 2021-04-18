b745669cd52bf21b93c8edea1b30a0d180637e9d
Skayo <jonas@skayo.dev>
2021-04-18 11:27:31 +0200

    Added install/uninstall scripts

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+composer install
+npm install
+yay -S nss atk at-spi2-atk libcups libdrm libxkbcommon libxcomposite libxdamage libxrandr gtk3
---
+++ uninstall.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+rm -rf node_modules
+yay -R nss atk at-spi2-atk libcups libdrm libxkbcommon libxcomposite libxdamage libxrandr gtk3
